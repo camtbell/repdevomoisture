35fb471ff39470c159fde27535693d9bbed4d59a
Cameron Bell <132916304+camtbell@users.noreply.github.com>
2023-09-05 03:16:51 -0300

    More tinkering with contour file
    Added personal notes and pasted some functions from other models.

--- 2_code/contour_example.r
+++ 2_code/contour_example.r
@@ -1,12 +1,20 @@
+library(ggplot)
+library(gmcontour)
+library(mcgv)
+
 # Create a dataframe used for predcitions. Note that these MUST be names exactly the same as the data and be in the sam units as the data used to fit the model. So if you centred then they must be centered also. 
 newdata <- data.frame(temp_diff = rep(seq(15, 36, by = 1), each = 12), moist_diff = seq(100, 320, by = 20))
 
 # Then make predcitions
 preds <- predict(metamean, newdata = newdata, transf = exp, digits = 2)
 
-# Then you use this griant dataframe with x, y and z values to make a contour plot
+# Then you use this griant dataframe with x (temperature), y (kPa diff) and z (maybe effect size?) values to make a contour plot
 
-#Cam's Attempt. ############################################################################################
+###############################Cam's Attempt.###############################################################
+#Will be based off sex ratio; moderators being Temperature vs. kPa sig interaction.
+#Therefore:
+Sex_dataOR <- read_csv("3_trait data/sex_data.csv")
+
 newdata <- data.frame(temp_diff = rep(seq(15, 36, by = 1), each = 12), moist_diff = seq(100, 320, by = 20))
 #Mean Centring, taken from survival_model.R
 Sur_dataOR$T_scaled <- (Sur_dataOR$T - 25) 
