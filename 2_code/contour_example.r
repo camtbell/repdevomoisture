c89c7546bd229b65aa075a62be19f3feffffe85c
Cameron Bell <132916304+camtbell@users.noreply.github.com>
2023-09-14 17:25:36 -0300

    Set Maximum Limit of Water Diff Scaled
    Removed line 35 from the dataframe, as it contained a waterpotdiff_scaled value (4675) that was throwing the graph out of wack. Unfortunately did not solve our points/real data on the graph problem.

--- 2_code/contour_example.r
+++ 2_code/contour_example.r
@@ -1,11 +1,13 @@
+#######################################Contour Plot - Sex Ratio##############################################################
 # 1. Clear R workspace and load data
 	rm(list=ls())
+	#Loading packages
 	pacman::p_load(interp, metaAidR, metafor, cowplot, hrbrthemes, corrplot, ggpubr, ggplot2, MCMCglmm, ape, phytools, stats, rotl, readr, MuMIn, clubSandwich, dplyr, viridis, patchwork, orchaRd, mgcv, interp, akima, fields, MASS)
-
+  #Loading data
 	Sex_dataOR <- read_csv("3_trait data/sex_data.csv")
-
+  #Calculate effect size
 	Sex_dataOR <- escalc(measure="OR", ai=male.2, bi=female.2, ci =male, di=female , data=Sex_dataOR)
-
+  #Scale temperature and difference in water potential values (mean centering)
 	Sex_dataOR$T_scaled <- (Sex_dataOR$T - 25) 
 	Sex_dataOR$waterpotdiff_scaled <- (Sex_dataOR$waterpot_diff - 320)
 
@@ -37,16 +39,18 @@
 	tl2_brlen$node.label <- NULL
 	R_phylo <- vcv(tl2_brlen, corr = TRUE)
 
-## 4. Final model based on AICc selection top model - see .R file for more details. We'll drop order through so predcitions can be marginalised over order
+## 4. Final model based on AICc selection top model - see .R file for more details. We'll drop order through so predictions can be marginalized over order
 final.model <-rma.mv(yi = yi, V = Vmat, random = list(~1|paper_no, ~1|animal, ~1|row_count), R = list(animal=R_phylo), mod = ~ T_scaled + waterpotdiff_scaled:T_scaled + waterpotdiff_scaled, data =Sex_dataOR, method = 'REML')
 final.modelb <- robust(final.model, cluster = Sex_dataOR$paper_no)
 summary(final.modelb)
 
-# Create a dataframe used for predcitions. Note that these MUST be names exactly the same as the data and be in the sam units as the data used to fit the model. So if you centred then they must be centered also. 
+## 5. Create a dataframe used for predictions. Note that these MUST be names exactly the same as the data and be in the same units as the data used to fit the model. So if you centered then they must be centered also. 
+#Removing Row 35, had a wildly high waterpot difference value (4675)
+Sex_dataOR <- Sex_dataOR[-35,]
 newdata <- as.matrix(data.frame(T_scaled = seq(min(Sex_dataOR$T_scaled), max(Sex_dataOR$T_scaled), length.out = 100), 
 					  waterpotdiff_scaled = rep(seq(min(Sex_dataOR$waterpotdiff_scaled), max(Sex_dataOR$waterpotdiff_scaled), length = 100), each = 100))  %>% mutate(`T_scaled:waterpotdiff_scaled` = T_scaled*waterpotdiff_scaled))
-
-# Then make predcitions
+# Then make predictions
+#So i can set limits on this line? Might be an issue with final.model. check preds data...
 preds <- data.frame(predict(final.model, newmods = newdata, digits = 2, addx=TRUE))
 
 
@@ -68,8 +72,7 @@ s <- interp(x = preds$X.T_scaled, y = preds$X.waterpotdiff_scaled, z = preds$pre
 image.plot(s, xlab = "Temperature (centered)", ylab = "Moisture Difference (Centered)", las = 1, col = viridis(option = "magma", 50), main = "Sex Ratio", cex.main = 2, cex.axis = 1, axis.args = list(cex.axis = 1.5), cex.lab = 1.8)
 contour(s, add = TRUE, col = "white")
 points(y = Sex_dataOR$T_scaled, x = Sex_dataOR$waterpotdiff_scaled)
-#I believe 'points' ^^^ is whats not working for my data, just not adding the dots, only showing trend
-############################################################################################################
+#############################################################################################################################
 
 # Figure 6 - Model predictions - traits - raw
 #------------------------------------------------------------------
